fef145f3e738e434b08d1934fbff6f57f668d905
usmhic <usama.hicho.mail@gmail.com>
2023-03-16 02:02:50 +0100

    update

--- democode/Program.cs
+++ democode/Program.cs
@@ -1,6 +1,8 @@
 using democode;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+    using Radzen;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
